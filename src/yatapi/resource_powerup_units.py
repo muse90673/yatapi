c75155a46b74955486d43a5eb8c2c915ec5231d5
muse90673 <906731085@qq.com>
2023-12-03 19:15:14 +0800

    Replace the instance creation in resource/unkillable unit list with specific constants.

--- src/yatapi/resource_powerup_units.py
+++ src/yatapi/resource_powerup_units.py
@@ -1,12 +1,14 @@
-from yatapi.scunit import SCUnit
+__all__ = ('RESOURCE_UNITS',)
+
+from yatapi import scunit
 
 RESOURCE_UNITS = [
- SCUnit('\"Mineral Chunk (Type 1)\"'),
- SCUnit('\"Mineral Chunk (Type 2)\"'),
- SCUnit('\"Vespene Orb (Protoss Type 1)\"'),
- SCUnit('\"Vespene Orb (Protoss Type 2)\"'),
- SCUnit('\"Vespene Sac (Zerg Type 1)\"'),
- SCUnit('\"Vespene Sac (Zerg Type 2)\"'),
- SCUnit('\"Vespene Tank (Terran Type 1)\"'),
- SCUnit('\"Vespene Tank (Terran Type 2)\"')
+    scunit.MINERAL_CHUNK_TYPE_1,
+    scunit.MINERAL_CHUNK_TYPE_2,
+    scunit.VESPENE_ORB_PROTOSS_TYPE_1,
+    scunit.VESPENE_ORB_PROTOSS_TYPE_2,
+    scunit.VESPENE_SAC_ZERG_TYPE_1,
+    scunit.VESPENE_SAC_ZERG_TYPE_2,
+    scunit.VESPENE_TANK_TERRAN_TYPE_1,
+    scunit.VESPENE_TANK_TERRAN_TYPE_2,
 ]
